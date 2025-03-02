3865959ae60c9841c82cb643b31c0b2faab72e0f
AhmedFayed123 <medofayed683@gmail.com>
2025-03-02 12:49:08 +0200

    Deploy to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "53edcf8e421814fb0eeeabbe3b723428
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "1d122b99c07de4dcdeb798fdcd528a19",
+"flutter_bootstrap.js": "8fffa312afa4256236cf1aa460f4f904",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
